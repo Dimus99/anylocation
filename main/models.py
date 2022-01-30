77690693634b9c76323bdd8a26baaa3a9c076960
Dimus99 <shelpyakovd@bk.ru>
2022-01-30 16:24:09 +0500

    add models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    img = models.ImageField()
+    additional_img = models.ImageField(blank=True, null=True)
+    description = models.CharField(max_length=1023, default="")
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class ProductVariant(models.Model):
+    id = models.AutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"name: {self.name}, product: {self.product.name}"
--- main/views.py
+++ main/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-
 # Create your views here.
+from main import models
+from main.models import Product
+
 
 def index(request):
-    return HttpResponse("aaaa")
+    a = Product.objects.all()
+    print(a)
+    return HttpResponse(a[0].productvariant_set.all())
+    # return render(request, template_name="main.html")
