71c0449c842d2d95c56402024033cfe7068b4ff7
Dimus99 <shelpyakovd@bk.ru>
2022-02-19 20:32:48 +0500

    fuck2

--- main/models.py
+++ main/models.py
@@ -25,7 +25,7 @@ class ProductType(models.Model):
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
-    cost = models.IntegerField()
+    cost = models.CharField(max_length=50, null=True)
     img = models.ImageField()
     additional_img = models.ImageField(blank=True, null=True)
     description = models.CharField(max_length=1023, default="")
