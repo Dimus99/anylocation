1805aaaa643b650d296645417c1fde6d683e1215
Dimus99 <shelpyakovd@bk.ru>
2022-02-19 21:01:24 +0500

    edit views and models

--- main/views.py
+++ main/views.py
@@ -3,12 +3,16 @@ from django.shortcuts import render, HttpResponse
 from main.models import Product, ProductType, Page
 
 
+def main(request):
+    return HttpResponse("main page")
+
+
 def index(request):
     url = request.path[1:]
     page = Page.objects.filter(slug=url).first()
     resp = None
     if page:
-        types = page.product_type_set()
+        types = page.producttype_set.all()
         resp = get_page(request, page.slug, types)
     return resp if resp else HttpResponse("error" + url)
 
@@ -17,5 +21,35 @@ def index(request):
 # возвращает отрендеренную страницу
 def get_page(request, header, types):
     # header + main + footer
+    serial_types = [
+        {"name": product_type.name,
+         "description": product_type.description,
+         "products": serialize_products(product_type.product_set.all())
+         }
+        for product_type in types
+    ]
+    data = {"header": header, "types": serial_types}
+    return render(request, "body.html", context=data)
 
-    return HttpResponse(str([str(i) for i in types]))
+
+def serialize_variants(variants):
+    return [
+        {
+            "name": v.name,
+            "price": v.price
+        }
+        for v in variants
+    ]
+
+
+def serialize_products(products):
+    return [
+        {
+            "name": n.name,
+            "description": n.description,
+            "cost": n.cost,
+            "img": n.img.path,  # TODO: need serialize
+            "additional_img": n.additional_img,
+            "variants": serialize_variants(n.productvariant_set.all())
+        } for n in products
+    ]
